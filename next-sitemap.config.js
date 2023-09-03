e7a5a93788731fc5918fe2d4820653741b56da00
SMEW Technology <vn.apnic@gmail.com>
2023-09-03 16:17:49 +0700

    fixed js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://smew.techyar',
+  siteUrl: process.env.SITE_URL || 'https://smew.tech',
   generateRobotsTxt: true, // only generate robots.txt for prod
   // ...other options
 };
